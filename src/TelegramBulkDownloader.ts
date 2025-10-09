b21aa8a4c47d225e4b44245696e688b64e44d8db
andpon1977 <andrea.poncato@gmail.com>
2025-10-09 05:13:19 +0200

    Update TelegramBulkDownloader.ts

--- src/TelegramBulkDownloader.ts
+++ src/TelegramBulkDownloader.ts
@@ -120,7 +120,7 @@ class TelegramBulkDownloader {
   }
 
   while (true) {
-    const maxMessagesForCycle = 1000;
+    const maxMessagesForCycle = 10000;
     const topicFilter = process.argv[2]; // esempio "10" oppure undefined
     type Message = any;
     let messages: Message[] = [];
